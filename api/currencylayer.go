7b8257e65218fc7b5004e3819185ed6e2adb983d
TobiasSchnizel <men520054@hotmail.com>
2022-01-07 13:34:45 -0400

    layer consumption api

--- api/currencylayer.go
+++ api/currencylayer.go
@@ -1,34 +1,41 @@
 package api
-import (
- "encoding/json"
- "fmt"
- "io/ioutil"
- "net/http"
-)
-type Response struct {
- ID     string `json:"id"`
- Joke   string `json:"joke"`
- Status int    `json:"status"`
-}
-func CurrencyConsult() {
 
-client := &http.Client{}
- req, err := http.NewRequest("GET", "https://icanhazdadjoke.com/", nil)
- if err != nil {
-  fmt.Print(err.Error())
- }
- req.Header.Add("Accept", "application/json")
- req.Header.Add("Content-Type", "application/json")
- resp, err := client.Do(req)
-defer resp.Body.Close()
- bodyBytes, err := ioutil.ReadAll(resp.Body)
- if err != nil {
-  fmt.Print(err.Error())
- }
-var responseObject Response
- json.Unmarshal(bodyBytes, &responseObject)
- fmt.Printf("API Response as struct %+v\n", responseObject)
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type Response struct {
+	ID     string `json:"id"`
+	Joke   string `json:"joke"`
+	Status int    `json:"status"`
 }
+
+/*success	true
+terms	"https://currencylayer.com/terms"
+privacy	"https://currencylayer.com/privacy"
+timestamp	1641543075
+source	"USD"
+quotes
+USDEUR	0.884795
+USDGBP	0.738935
+USDCAD	1.27299
+USDPLN	4.032102*/
+func CurrencyConsult() {
+	jsonData := map[string]string{"firstname": "Nic", "lastname": "Raboy"}
+	jsonValue, _ := json.Marshal(jsonData)
+	response, err = http.Post("https://httpbin.org/post", "application/json", bytes.NewBuffer(jsonValue))
+	if err != nil {
+		fmt.Printf("The HTTP request failed with error %s\n", err)
+	} else {
+		data, _ := ioutil.ReadAll(response.Body)
+		fmt.Println(string(data))
+	}
+}
+
 /*access_key=8fd97f726bfc7c0d8926297498d35e01
 currencies=EUR,GBP,CAD,PLN
 source=USD
